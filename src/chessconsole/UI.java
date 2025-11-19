915b65022ec32530f1a0f9c7649682c2b9a3506a
EIJARRA <josearennan@gmail.com>
2025-11-18 22:26:36 -0300

    Implement feature to visualize possible moves on the board
    - Update 'UI' class to support background colors (ANSI_BLUE_BACKGROUND).
    - Create overloaded 'UI.printBoard' method to highlight possible moves.
    - Add 'possibleMoves' method in 'ChessMatch' to delegate logic to the board.
    - Update 'Program' loop to calculate and display possible moves before the target input.

--- src/chessconsole/UI.java
+++ src/chessconsole/UI.java
@@ -8,6 +8,7 @@ import chess.ChessPosition;
 
 public class UI {
 
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
@@ -47,6 +48,18 @@ public class UI {
         System.out.println("  a b c d e f g h");
     }
 
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < pieces[i].length; j++) {
+
+                printPiece(pieces[i][j], possibleMoves[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
+
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
             System.out.print("-" + ANSI_RESET);
@@ -59,5 +72,27 @@ public class UI {
             }
         }
         System.out.print(" ");
+    }
+
+    private static void printPiece(ChessPiece piece, boolean background) {
+        if (background) {
+            System.out.print(ANSI_BLUE_BACKGROUND);
+        }
+        if (piece == null) {
+            System.out.print("-" + ANSI_RESET);
+        }
+        else {
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            } else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
